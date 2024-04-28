4fb8ad73b1fe3a6e01775579a6ac4ec3748a1de4
tomasfratrik <tomasfratrik8@gmail.com>
2024-04-28 15:36:17 +0200

    remove dead code

--- src/components/Dropzone.vue
+++ src/components/Dropzone.vue
@@ -1,13 +1,4 @@
 <template>
-    <!-- <n-button 
-        :disabled="!fileListLength" 
-        style="margin-bottom: 12px;"
-        class="search-button"
-        @click="handleClick"
-        >
-
-        Search
-    </n-button> -->
     <loading v-model:active="isLoading"
         :can-cancel="false"
         :color="spinColor"
@@ -96,94 +87,9 @@ const clearFileList = () => {
 
 </script>
 
-<!-- <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { NUpload, NIcon, NP, NText, NUploadDragger, NButton } from 'naive-ui'
-import { ArchiveOutline as ArchiveIcon } from '@vicons/ionicons5'
-import type { UploadInst, UploadFileInfo } from 'naive-ui'
-import { showErrorToast } from '@/utils/toast'
-import { serverAddress } from '@/utils/server'
-import Loading from 'vue-loading-overlay';
-import axios from 'axios'
-
-
-export default defineComponent({
-    components: {
-        NUpload,
-        NIcon,
-        NP,
-        NText,
-        NUploadDragger,
-        NButton,
-        ArchiveIcon
-    },
-    setup () {
-        const fileListLengthRef = ref(0)
-        const fileListDisabled = ref(false)
-        const uploadRef = ref<UploadInst | null>(null)
-        const uploadedImages = ref<UploadFileInfo[]>([])
-        const result = ref('')
-
-        const handleChange = (data: { fileList: UploadFileInfo[] }) => {
-            fileListLengthRef.value = data.fileList.length
-            uploadedImages.value = data.fileList
-            uploadImg()
-        }
-
-        const uploadImg = async () => {
-
-            const fileObject = uploadedImages.value[0];
-
-            const file = fileObject.file;
-            const formData = new FormData();
-            const blob = file as Blob;
-            formData.append('file', blob);
-
-
-            try {
-                const response = await axios.post(`${serverAddress}/grisa/upload`, formData);
-                const data = response.data
-
-                clearFileList()
-                if (data.error) {
-                    showErrorToast(data.error)
-                    return
-                }
-                console.log(data)
-          
-            } catch (error) {
-                console.error('ERROR:' + error);
-                clearFileList()
-            }
-        }
-
-        const clearFileList = () => {
-            uploadedImages.value = []
-            fileListLengthRef.value = 0
-            uploadRef.value?.clear()
-        }
-
-        return {
-            upload: uploadRef,
-            fileListLength: fileListLengthRef,
-            handleChange,
-            uploadImg,
-            fileListDisabled,
-            clearFileList,
-            uploadedImages
-        }
-    }
-})
-
-</script> -->
 
 <style scoped>
 .search-button {
     width: 100%;
-}
-
-.search-button:hover {
-    /* color: var(--primary-color);
-    border-color: var(--primary-color); */
 }
 </style>
